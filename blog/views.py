01b69f5d0bb3bcc4e90f4435faaf87ffbbcf4c51
kevin-rayan <kevinrayancc@gmail.com>
2022-09-16 13:06:43 -0300

    novas templates

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.shortcuts import HttpResponse
 
 # Create your views here.
+
+def chiclete (request):
+    return render(request, template_name='blog/home.html')
--- projeto/urls.py
+++ projeto/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from blog.views import chiclete
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('chiclete/', chiclete), 
 ]
